734f19abd00aeff4fa564ee1500c56e6283b654b
Fernando Lira <it.fernandolira@gmail.com>
2023-12-03 18:48:02 +0000

    Extra CSS rules

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import Primeiro from "./components/basic/Primeiro";
 import Segundo from "./components/basic/Segundo";
 import Card from "./components/layout/Card";
+import './App.css';
 
 const app = () => {
     return (
-        <div>
-            <Card title="Primeiro">
-                <Primeiro />
-                <Primeiro />
-            </Card>
-            <Card title="Segundo">
-                <Segundo />
-                <Segundo />
-            </Card>
+        <div className="App">
+            <div className="Cards">
+                <Card title="Primeiro">
+                    <Primeiro />
+                    <Primeiro />
+                </Card>
+                <Card title="Segundo">
+                    <Segundo />
+                    <Segundo />
+                </Card>
+            </div>
         </div>
     )
 }
