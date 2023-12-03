86bf25c3ee2c4f616a586dfa3653e50b28d62715
Fernando Lira <it.fernandolira@gmail.com>
2023-12-03 19:24:07 +0000

    Conditional Render - part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Primeiro from "./components/basic/Primeiro";
 import Segundo from "./components/basic/Segundo";
 import Card from "./components/layout/Card";
+import ParOuImpar from "./components/basic/ParOuImpar";
 import './App.css';
 
 const app = () => {
@@ -14,6 +15,10 @@ const app = () => {
                 <Card title="Segundo" color="#DE3163">
                     <Segundo />
                     <Segundo />
+                </Card>
+                <Card title="Condicional #1" color="#FF00FF">
+                    <ParOuImpar numero={16} />
+                    <ParOuImpar numero={7} />
                 </Card>
             </div>
         </div>
---
+++ src/components/basic/ParOuImpar.js
@@ -0,0 +1,11 @@
+const parOuImpar = props => {
+    const ePar = props.numero % 2 === 0;
+    return (
+        <div>
+            <span>Número: {props.numero} - </span>
+            {ePar ? <span>Par</span> : <span>Ímpar</span>}
+        </div>
+    )
+}
+
+export default parOuImpar;
